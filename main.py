0ee014f377519e29c9cd2962d3070af28042f90a
QuantumBen <omosabenedict@gmail.com>
2025-09-04 05:14:35 -0700

    To run it locally ".venv\Scripts\Activate" then "fastapi dev .\main.py"

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI(root_path="/api/v1")
 
@@ -9,21 +10,21 @@ async def root():
 data = [
     {
         "campaign_id": 1,
-        "name": "Campaign 1",
-        "due_date": "2024-12-31T23:59:59",
-        "created_at": "2024-01-01T12:00:00"
+        "name": "Summer Launch",
+        "due_date": datetime.now(),
+        "created_at": datetime.now()
     },
     {
         "campaign_id": 2,
-        "name": "Campaign 2",
-        "due_date": "2024-11-30T23:59:59",
-        "created_at": "2024-02-01T12:00:00"
+        "name": "Black Friday",
+        "due_date": datetime.now(),
+        "created_at": datetime.now()
     }
     ,{
         "campaign_id": 3,
         "name": "Campaign 3",
-        "due_date": "2024-10-31T23:59:59",
-        "created_at": "2024-03-01T12:00:00"
+        "due_date": datetime.now(),
+        "created_at": datetime.now()
     }
 ]
 
@@ -38,3 +39,5 @@ Campaign
 @app.get("/campaigns")
 async def read_campaigns():
     return {"campaigns": data}
+
+
