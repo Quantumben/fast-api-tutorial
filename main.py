b4a5c93ea25c0daf7691a258701590ce5bbec83b
QuantumBen <omosabenedict@gmail.com>
2025-09-04 05:56:46 -0700

    create campaigns endpoint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from datetime import datetime
 from typing import Any
+from random import randint
 app = FastAPI(root_path="/api/v1")
 
 @app.get("/")
@@ -47,3 +48,15 @@ async def read_campaign(campaign_id: int):
         if campaign["campaign_id"] == campaign_id:
             return {"campaign": data[campaign_id - 1]}
     raise HTTPException(status_code=404, detail="Campaign not found")
+
+@app.post("/campaigns")
+async def create_campaign(body: dict[str, Any]):
+    new : Any = {
+        "campaign_id": randint(100, 1000),
+        "name": body.get("name"),
+        "due_date": body.get("due_date"),
+        "created_at": datetime.now()
+    }
+
+    data.append(new)
+    return {"campaign": new}
