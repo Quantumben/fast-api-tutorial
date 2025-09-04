fb4a918dab24124711fc61aa70934357419197e2
QuantumBen <omosabenedict@gmail.com>
2025-09-04 04:59:59 -0700

    Added a new endpoint

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ app = FastAPI(root_path="/api/v1")
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+"""
+Campaign
+- campaign_id: int
+- name: str
+- due_date: datetime
+- created_at: datetime
+"""
+
+@app.get("/campaigns")
+async def read_campaigns():
+    return {"campaigns": "example"}
