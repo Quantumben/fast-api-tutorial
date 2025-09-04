fd04a4377b79868152152053b49572e98d2339c1
QuantumBen <omosabenedict@gmail.com>
2025-09-04 06:56:26 -0700

    Connect main.py to DB

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-from fastapi import FastAPI, HTTPException, Request, Response
+from fastapi import FastAPI, HTTPException, Request, Response, Depends
 from datetime import datetime
-from typing import Any
+from typing import Any, Annotated
 from random import randint
-app = FastAPI(root_path="/api/v1")
+from sqlmodel import SQLModel, create_engine, Session
+from contextlib import asynccontextmanager
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(root_path="/api/v1", lifespan=lifespan)
 
 @app.get("/")
 async def root():
